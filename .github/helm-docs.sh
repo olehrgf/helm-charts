c60ad8767638952ae5fd844e2c1a15df1ba71b86
olehrgf <olehrgf@gmail.com>
2023-09-14 19:10:08 +0300

    Update helm doc generation pipeline

--- .github/helm-docs.sh
+++ .github/helm-docs.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -xe
 
-CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/main -- 'charts' | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
+CHART_DIRS="$(git log -m -1 --name-only --pretty="format:" "$(git rev-list --no-merges -n 1 HEAD)" -- 'charts' | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
+
 if [[ -z "$CHART_DIRS" ]]; then
     echo "No Chart.yaml changes detected, aborting helm-docs"
     exit 1
 fi
 
 for CHART_DIR in ${CHART_DIRS}; do
-  ./helm-docs -c ${CHART_DIR}
+  helm-docs -c ${CHART_DIR}
 done
